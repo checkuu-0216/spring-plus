48e70ed9f3487c78b1fac673740d482227f31842
checkuu-0216 <corb37@gmail.com>
2024-10-03 21:47:45 +0900

    level2.6 cascade 추가

--- src/main/java/org/example/expert/domain/manager/entity/Manager.java
+++ src/main/java/org/example/expert/domain/manager/entity/Manager.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.user.entity.User;
+import org.hibernate.annotations.Cascade;
 
 @Getter
 @Entity
@@ -16,7 +17,7 @@ public class Manager {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false) // 일정 만든 사람 id
+    @JoinColumn(name = "user_id", nullable = false)// 일정 만든 사람 id
     private User user;
     @ManyToOne(fetch = FetchType.LAZY) // 일정 id
     @JoinColumn(name = "todo_id", nullable = false)
