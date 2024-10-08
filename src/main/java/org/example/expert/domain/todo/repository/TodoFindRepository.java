754e32a33315cbee47a5cf19b0cb1f23295b8569
checkuu-0216 <corb37@gmail.com>
2024-10-08 22:43:59 +0900

    level3.10 코드 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoFindRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoFindRepository.java
@@ -1,9 +1,20 @@
 package org.example.expert.domain.todo.repository;
 
+import com.querydsl.core.types.dsl.Wildcard;
+import org.example.expert.domain.comment.dto.response.CommentResponse;
+import org.example.expert.domain.manager.dto.response.ManagerResponse;
+import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TodoFindRepository {
     Todo findByIdWithUserFromQueryDsl(long todoId);
+
+    Page<TodoResponse> search(Pageable pageable, Long managerId);
+
 }
--- src/main/java/org/example/expert/domain/todo/repository/TodoFindRepositoryImpl.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoFindRepositoryImpl.java
@@ -1,11 +1,22 @@
 package org.example.expert.domain.todo.repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.comment.dto.response.CommentResponse;
+import org.example.expert.domain.manager.dto.response.ManagerResponse;
+import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import static org.example.expert.domain.comment.entity.QComment.comment;
+import static org.example.expert.domain.manager.entity.QManager.manager;
 import static org.example.expert.domain.todo.entity.QTodo.todo;
 import static org.example.expert.domain.user.entity.QUser.user;
 
@@ -22,6 +33,36 @@ public class TodoFindRepositoryImpl implements TodoFindRepository{
                 .leftJoin(todo.user,user)
                 .where(todoIdEq(todoId))
                 .fetchOne();
+    }
+    @Override
+    public Page<TodoResponse> search(Pageable pageable, Long managerId) {
+        List<Todo> results = queryFactory
+                .select(todo)
+                .distinct()
+                .from(todo)
+                .leftJoin(todo.managers, manager).fetchJoin()
+                .leftJoin(todo.comments, comment)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long totalCount = queryFactory
+                .select(Wildcard.count)
+                .from(todo)
+                .fetchOne();
+
+        List<TodoResponse> content = results.stream()
+                .map(todo ->
+                        new TodoResponse(
+                                todo.getId(),
+                                todo.getTitle(),
+                                todo.getContents(),
+                                todo.getComments().stream().map(comment -> new CommentResponse(comment.getId(), comment.getContents())).toList(),
+                                todo.getManagers().stream().map(manager -> new ManagerResponse(manager.getId(), manager.getUser())).toList()
+                        )
+                ).toList();
+
+        return new PageImpl<>(content, pageable, totalCount);
     }
 
     private BooleanExpression todoIdEq(Long todoId) {
